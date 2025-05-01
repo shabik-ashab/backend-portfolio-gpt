9ee79b29bf8f581733b01f046d22e1c43e0097e2
shabik-ashab <shabik.ashab@gmail.com>
2025-05-01 13:22:45 +0600

    feat(auth): add login and register route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.routes import resume
+from app.routes import auth
+
+
 
 app = FastAPI()
 app.include_router(resume.router)
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
